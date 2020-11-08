42150ea48dbd510a3a96533440874c35fcd77f12
Snyu.L <1012439415@qq.com>
2020-11-08 15:34:45 +0800

    day01

---
+++ src/components/Login.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="Login-container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        class="demo-ruleForm"
+      >
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user"
+            v-model="ruleForm.username"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            type="password"
+            v-model="ruleForm.password"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item class="btn-box">
+          <el-button size="small" type="primary" @click="submitForm('ruleForm')"
+            >登录</el-button
+          >
+          <el-button size="small" @click="resetForm('ruleForm')"
+            >重置</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        username: "admin",
+        password: "123456"
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6到 12 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log(this, 999);
+          // console.log(this.ruleForm, 777);
+          //  如果valid为true   表示校验满足规则  可以发送请求
+          this.$axios.post("/login", this.ruleForm).then(res => {
+            console.log(res, 666);
+
+            //如果登录成功则会转到home界面，否则弹出提示框
+            // console.log(res.status);
+            if (res.data.meta.status == 200) {
+              this.$message.success("恭喜你登录成功");
+              // this.$router.push("/Home");
+            } else {
+              this.$message.error(res.data.meta.msg);
+            }
+          });
+
+          // alert("登录成功!");
+        } else {
+          console.log("错误密码!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+<style scoped lang="less">
+.Login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  width: 100%;
+  .login_box {
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    width: 450px;
+    height: 300px;
+    border-radius: 3px;
+    top: 50%;
+  }
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    -webkit-box-shadow: 0 0 10px #ddd;
+    box-shadow: 0 0 10px #ddd;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .el-form {
+    margin-top: 80px;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .btn-box {
+    text-align: right;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "../src/global/common.css";
+import axios from "axios";
+axios.defaults.baseURL = "http://klxin.cn:8888/api/private/v1";
+// console.log(Vue, 8888);
 
-Vue.config.productionTip = false
+//  原型对象上挂载  axios
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app");
